5ad1a0ead360bd66e9e1bb956c668583132f110f
aztilux <aztilux@gmail.com>
2025-11-09 21:51:26 +0100

    Update base path in astro.config.mjs to root

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://aztilux.github.io/',
-    base: '/personal2/',
+    base: '/',
     vite: {
         plugins: [tailwindcss()]
     }
